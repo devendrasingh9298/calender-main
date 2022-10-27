16249409749fcce6b531ccba660063f07bd9da33
Devendra Singh Chauhan <107784356+devendrasingh9298@users.noreply.github.com>
2022-10-27 13:17:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+var dt = new Date();
+function RenderDate() {
+  var day = dt.getDay();
+  var endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
+  var prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();
+
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const date = new Date();
+  let today = date.getDate();
+  // console.log(today);
+  document.getElementById("date_str").innerHTML = dt.toDateString();
+  document.getElementById("month").innerHTML = months[dt.getMonth()];
+  var cells = "";
+
+  for (x = day; x > 0; x--) {
+    cells += "<div class='prev_date' >" + (prevDate - x + 1) + "</div>";
+  }
+  
+  for (i = 1; i <= endDate; i++) {
+    if (i == today && dt.getMonth() == dt.getMonth()) {
+      cells += "<div class='today'>" + i + "</div>";
+    } else {
+      cells += "<div>" + i + "</div>";
+    }
+  }
+  document.getElementsByClassName("days")[0].innerHTML = cells;
+}
+function moveDate(para) {
+  if (para == "prev") {
+    dt.setMonth(dt.getMonth() - 1);
+  } else if (para == "next") {
+    dt.setMonth(dt.getMonth() + 1);
+  }
+  RenderDate();
+}
